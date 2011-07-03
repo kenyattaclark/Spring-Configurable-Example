a125dd3034c8cbdcfaf423040c61260adc093e3f
Kenyatta Clark <kenyatta.clark@gmail.com>
2011-07-02 20:36:28 -0500

    Missed commit ssed#

--- src/main/java/com/brightdome/sample/spring/HelloWorldClient.java
+++ src/main/java/com/brightdome/sample/spring/HelloWorldClient.java
@@ -1,7 +1,9 @@
 package com.brightdome.sample.spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
 
+@Configurable
 public class HelloWorldClient {
 	
 	@Autowired
